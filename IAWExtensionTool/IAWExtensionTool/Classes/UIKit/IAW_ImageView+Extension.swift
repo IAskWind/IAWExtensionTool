77feacff06487e9ec9fac9dc999eeb8c03dfca95
IAskWind <330262107@qq.com>
2017-07-10 17:18:18 +0800

    添加图片旋转方法

--- IAWExtensionTool/IAWExtensionTool/Classes/UIKit/IAW_ImageView+Extension.swift
+++ IAWExtensionTool/IAWExtensionTool/Classes/UIKit/IAW_ImageView+Extension.swift
@@ -27,4 +27,20 @@ public extension UIImageView {
     
     
     }
+    
+    // 360度旋转图片
+    func rotate360Degree() {
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z") // 让其在z轴旋转
+        rotationAnimation.toValue = NSNumber(value: .pi * 2.0) // 旋转角度
+        rotationAnimation.duration = 0.6 // 旋转周期
+        rotationAnimation.isCumulative = true // 旋转累加角度
+        rotationAnimation.repeatCount = MAXFLOAT // 旋转次数
+        layer.add(rotationAnimation, forKey: "rotationAnimation")
+    }
+    
+    // 停止旋转
+    func stopRotate() {
+        layer.removeAllAnimations()
+    }
+
 }
